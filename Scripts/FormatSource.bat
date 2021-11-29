0e20c453ceed7b24d8dd71c88cf49fd628e2af99
cvet <cvet@tut.by>
2021-11-29 23:10:45 +0300

    Scripts formatting

--- Scripts/FormatSource.bat
+++ Scripts/FormatSource.bat
@@ -5,9 +5,3 @@ FOR /F "tokens=*" %%G IN ('DIR /B /S .\*.fos') DO (
 	"SourceTools\uncrustify.exe" -f "%%G" -c "SourceTools\uncrustify.cfg" -o indentoutput.tmp
 	MOVE /Y indentoutput.tmp "%%G"
 )
-
-FOR /F "tokens=*" %%G IN ('DIR /B /S .\*.fosh') DO (
-	ECHO Indenting file "%%G"
-	"SourceTools\uncrustify.exe" -f "%%G" -c "SourceTools\uncrustify.cfg" -o indentoutput.tmp
-	MOVE /Y indentoutput.tmp "%%G"
-)

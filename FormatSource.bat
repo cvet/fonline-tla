d318852c33b68ea304eefcb5195fbc895c5b49ee
cvet <cvet@tut.by>
2026-02-13 08:56:53 +0300

    Обновлен редактор интерфейса и формат файлов интерфейса

--- FormatSource.bat
+++ FormatSource.bat
@@ -1,3 +1,3 @@
 @echo off
 echo Scripts formatting
-cd "%~dp0" && "Tools/clang-format-20.exe" --verbose -i Scripts/*.fos Scripts/Json/*.fos Scripts/Extension/*.cpp
+cd "%~dp0" && "Tools/clang-format-20.exe" --verbose -i Scripts/*.fos Scripts/Json/*.fos Scripts/Extension/*.cpp Gui/*.fogui
